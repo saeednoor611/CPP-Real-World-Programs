65292dba674e90414e908c000bc0837cffa82920
Coding World <111302606+saeednoor611@users.noreply.github.com>
2022-12-28 20:04:35 -0800

    checking greater or less than numbers.cpp
    checking greater or less than numbers.cpp

---
+++ checking greater or less than numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	int num;
+	cout<<"Enter any number :";
+	cin>>num;
+	if(num>100)
+	{
+		cout<<"the number is greater than 100 :";
+	}
+	else 
+	{
+		cout<<"the number is less thab 100 :";
+	}
+}
