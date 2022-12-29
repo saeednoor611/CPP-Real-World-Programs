a7467619adf12d321ff280428ada3b6658be74c3
Coding World <111302606+saeednoor611@users.noreply.github.com>
2022-12-28 19:22:38 -0800

    array reverse order program.cpp
    array reverse order program.cpp

---
+++ array reverse order program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+//	array reverse order program
+    for(int i =1;i<=3;i++){
+    	cout<<"enter values"<<endl;
+    	cin>>i;
+	}
+	for(int i=3;i>=1;i--){
+		cout<<i;
+	}
+}
