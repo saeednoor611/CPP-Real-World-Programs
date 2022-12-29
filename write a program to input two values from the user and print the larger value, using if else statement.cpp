2a14e31e752afa65e264af94e9ffccdb71e1b3ee
Coding World <111302606+saeednoor611@users.noreply.github.com>
2022-12-28 20:06:00 -0800

    write a program to input two values from the user and print the larger value, using if else statement.cpp
    write a program to input two values from the user and print the larger value, using if else statement.cpp

---
+++ write a program to input two values from the user and print the larger value, using if else statement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a,b;
+	cout<<"Enter the value of A   ?"<<endl;
+	cin>>a;
+	cout<<"Enter the value of B    ?"<<endl;
+	cin>>b;
+	if(a>b)
+	{
+		cout<<"A is greater than B.";
+	}
+	else 
+	{
+		cout<<"A is less than B";
+	}
+}
